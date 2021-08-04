8594cf2f63eca0dd1d78160b19055d88cb24703b
Chris <chris.sun5567@email.com>
2021-08-04 22:18:15 +0000

    add email lookup function

--- express_server.js
+++ express_server.js
@@ -10,8 +10,8 @@ app.set("view engine", "ejs");
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
-const cookieParser = require('cookie-parser');
-app.use(cookieParser())
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 //login cookie
 app.post("/login", (req, res) => {
@@ -20,38 +20,37 @@ app.post("/login", (req, res) => {
   // console.log(req);
   res.redirect("/urls");
 });
-app.post('/logout', (req,res)=>{
+app.post("/logout", (req, res) => {
   const logout = req.body.logout;
-  res.clearCookie('username');
-  res.redirect ('/urls')
-})
+  res.clearCookie(user);
+  res.redirect("/urls");
+});
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-const users = { 
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
-  }
-}
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 //the new url page
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-   // username: req.cookies["username"],
+    // username: req.cookies["username"],
     user: users[req.cookies.user_id],
-   
-    };
-  res.render("urls_new",templateVars);
+  };
+  res.render("urls_new", templateVars);
 });
 
 //index
@@ -59,7 +58,7 @@ app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
     // username: req.cookies["username"],
-    user: users[req.cookies.user_id]
+    user: users[req.cookies.user_id],
   };
   res.render("urls_index", templateVars);
 });
@@ -69,8 +68,8 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-  //  username: req.cookies["username"],
-  user: users[req.cookies.user_id]
+    //  username: req.cookies["username"],
+    user: users[req.cookies.user_id],
   };
   // console.log(req.params)
   // console.log(req.params.shortURL)
@@ -79,7 +78,6 @@ app.get("/urls/:shortURL", (req, res) => {
   }
   res.render("urls_show", templateVars);
 });
-
 
 //Add -use the long url client provided to create a short url, add it to the database and show it in the redirected single url page.
 app.post("/urls", (req, res) => {
@@ -106,42 +104,55 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
-
-
 //generate 6 digit string
 function generateRandomString() {
   const shortURL = Math.random().toString(36).substr(2, 6);
   return shortURL;
 }
 
+//function: email lookup 
+function emailLookup(email) {
+  for (const id in users) {
+    if (users[id].email === email) {
+      console.log('matching email: ' + users[id].email)
+      console.log(`matching email: ${users[id].email}`)
+
+      return true;
+    }
+  }
+  return false;
+}
+
 //register page
-app.get('/register', (req,res)=>{
-    res.render("urls_register");
-})
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
 //registration handler
 app.post("/register", (req, res) => {
   const userID = generateRandomString();
-  console.log(userID)
+  if (!req.body.email || !req.body.password) {
+    res.status(400);
+    res.send('Empty email or password!')
+  }
+  if(emailLookup(req.body.email)){
+    res.send('Email already exists.') 
+  }
+  console.log(userID);
   users[userID] = {
     id: userID,
-    email:req.body.email,
-    password:req.body.password
-  }
-  console.log(users)
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(users);
   res.cookie("user_id", userID);
   res.redirect("/urls");
-    
 });
-
 
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
